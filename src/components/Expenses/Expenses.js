d663162dc11004cba9407967eb206fa2feef8a8b
Pramuditha <pramuditha.niroshan@circles.asia>
2022-10-19 16:18:42 +0530

    remove unwanted line

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
